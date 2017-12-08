5ec3199c5e110c123a5f6164afeda16c06d58a74
jsphLim <1806291589@qq.com>
2017-12-09 01:57:39 +0800

    Update PublishCallBack.java

--- src/com/ly/callback/PublishCallBack.java
+++ src/com/ly/callback/PublishCallBack.java
@@ -11,7 +11,6 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class PublishCallBack implements MqttCallback {
 
     public void connectionLost(Throwable cause) {
-        // 连接丢失后，一般在这里面进行重连
         System.out.println("连接断开，请重连");
     }
 
@@ -20,7 +19,6 @@ public class PublishCallBack implements MqttCallback {
     }
 
     public void messageArrived(String topic, MqttMessage message) throws Exception {
-        // subscribe后得到的消息会执行到这里面
         System.out.println("消息主题 : " + topic);
         System.out.println("消息Qos : " + message.getQos());
         System.out.println("消息内容 : " + new String(message.getPayload()));
